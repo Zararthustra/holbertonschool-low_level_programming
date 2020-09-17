96d0a1fa709ad4e8073562b2f4263561c0ca5206
Arthur <arthmayer@outlook.fr>
2020-09-17 17:30:09 +0000

    q10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int n = '0';
+	int n = 0;
 
 	while (n < 100)
 	{
-		putchar('0' + n / 10);
-		putchar('0' + n % 10);
+		putchar('0' + (n / 10));
+		putchar('0' + (n % 10));
 
 		if (n < 99)
 		{
-			putchar(' ');
 			putchar(',');
+			putchar(' ');
 		}
 	n++;
 	}
