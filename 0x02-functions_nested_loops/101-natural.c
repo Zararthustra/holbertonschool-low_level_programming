99ebf7762a5e32641087a09cbaa4503de048fc24
Arthur <arthmayer@outlook.fr>
2020-09-23 13:36:01 +0000

    advanced

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a = 3;
+	int c = 0;
+	int n;
+
+	n = 0;
+	while (n < 1023)
+	{
+		n = c * a;
+		printf("%d, ", n);
+		c++;
+	}
+	if (n = 1023)
+	{
+		
+	}
+	return (0);
+}
