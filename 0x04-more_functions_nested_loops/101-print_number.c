8c6ab0a7784d892dfec5a72f9435668ab3112049
Arthur <arthmayer@outlook.fr>
2020-09-25 16:04:24 +0000

    q12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,37 @@
-cccccc
+#include "holberton.h"
+
+/**
+ * print_number - prints a number
+ * @n: int to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	int pow;
+	unsigned int tmp;
+	int a;
+
+	pow = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		tmp = -n;
+	}
+	else
+	{
+		tmp = n;
+	}
+	while (tmp / pow > 9)
+	{
+		pow = pow * 10;
+	}
+	while (tmp > 9)
+	{
+		_putchar((tmp / pow) + '0');
+		a = (tmp / pow) * pow;
+		tmp = tmp - a;
+		pow = pow / 10;
+	}
+	_putchar(tmp + '0');
+}
