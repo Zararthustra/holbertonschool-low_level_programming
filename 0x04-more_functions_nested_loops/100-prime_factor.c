ea4e733f908f82e022648c02c9ecc8918d0ec520
Arthur <arthmayer@outlook.fr>
2020-09-25 12:20:34 +0000

    q11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,25 @@
-cccc
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	int x = 2;
+
+	while (x < n)
+	{
+		while (n % x == 0)
+		{
+			n = n / x;
+		}
+		x++;
+	}
+	printf("%d\n", x);
+	return (0);
+}
