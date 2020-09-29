5173cee7beaa45c068a910ab9abdba9114f5d57c
Arthur <arthmayer@outlook.fr>
2020-09-29 18:24:11 +0000

    q9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @src: source
+ * @dest: dest
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	return (dest);
+}
