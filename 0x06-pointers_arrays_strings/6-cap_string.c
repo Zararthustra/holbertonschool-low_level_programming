02e49ac8254b214af6cdca3d2615a0d37f977e92
Arthur <arthmayer@outlook.fr>
2020-10-02 08:14:23 +0000

    q6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,10 +2,29 @@
 
 /**
  * cap_string - capitalizes all words of a string
- * Return: 
+ * @s: string
+ * Return: char *
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int a;
+	int n;
 
+	a = 0;
+	while (s[a])
+	{
+		n = 0;
+		while (sep[n])
+		{
+			if ((a == 0 || s[a - 1] == sep[n]) && (s[a] >= 'a' && s[a] <= 'z'))
+			{
+				s[a] = s[a] - 32;
+			}
+			n++;
+		}
+		a++;
+	}
+	return (s);
 }
