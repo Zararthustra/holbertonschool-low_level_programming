c964ad38dc1db98efe79263f41a4de72fbe0017c
Arthur <arthmayer@outlook.fr>
2020-10-02 12:09:35 +0000

    q8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string
+ * Return: char *
+ */
+
+char *rot13(char *s)
+{
+	char a[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int x;
+	int y;
+
+	x = 0;
+	while (s[x])
+	{
+		y = 0;
+		while (y < 26)
+		{
+			if (s[x] == a[y])
+			{
+				s[x] = b[y];
+				break;
+			}
+			y++;
+		}
+		x++;
+	}
+	return (s);
+}
+
