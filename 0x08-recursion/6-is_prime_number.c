c0a85bad5bc0206e56990719cdf9b1cfabeca731
Arthur <arthmayer@outlook.fr>
2020-10-07 16:35:10 +0000

    q0 to q5

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - returns 1 if the input integer
+ * is a prime number, otherwise return 0
+ * @n: number
+ * Return: 1 if it is a prime number, else 0
+ */
+
+int is_prime_number(int n)
