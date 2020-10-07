c9b7d65aafa929a8f71bc665464b9603a05246ea
Arthur <arthmayer@outlook.fr>
2020-10-07 17:09:05 +0000

    q6

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ * @s: string
+ * Return: 1 if palindrome, else 0
+ */
+
+int is_palindrome(char *s)
+{
+
+}
