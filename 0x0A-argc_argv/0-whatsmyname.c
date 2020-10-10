83082c25a2186d70c6ad6998b6caea10d9ca2b38
Arthur <arthmayer@outlook.fr>
2020-10-10 13:00:12 +0000

    q3

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 
-/*
+/**
  * main - prints program name
  * @argc: argc
  * @argv: argv
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - main func
+ * @argc: argc
+ * @argv:argv
+ * Return: 0
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
