1cf481fc8fa361c958fd2b75b94aa2c2c1625a30
Arthur <arthmayer@outlook.fr>
2020-10-11 09:46:37 +0000

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,12 +6,12 @@
  * main - multiplies numbers
  * @argc: argc
  * @argv: argv
- * Return: 0
+ * Return: 0 success, else 1
  */
 
 int main(int argc, char *argv[])
 {
-	int mul = atoi(argv[1]) * atoi(argv[2]);
+	int mul;
 
 	if (argc != 3)
 	{
@@ -20,6 +20,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
+	mul = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", mul);
 	}
 	return (0);
