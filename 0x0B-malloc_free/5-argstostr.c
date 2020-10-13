81362636a457f3c894c4bae47ca9a9f712d753a6
Arthur <arthmayer@outlook.fr>
2020-10-13 16:46:35 +0000

    q5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the arguments of the program
+ * @ac: argc
+ * @av: argv
+ * Return: string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+		return (NULL);
+}
