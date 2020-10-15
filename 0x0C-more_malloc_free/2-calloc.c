df2b4e8c5ba65a0af453fe05691a50b5d1c93c50
Arthur <arthmayer@outlook.fr>
2020-10-15 12:02:17 +0000

    q2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,5 +10,26 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	int *a;
+	unsigned int i;
+	unsigned int j;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	a = malloc((nmemb + size) * sizeof(unsigned int));
+	if (a == NULL)
+		return (NULL);
+	i = 0;
+	while (i < nmemb)
+	{
+		j = 0;
+		while (j < size)
+		{
+			a[i] = 0;
+			j++;
+		}
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
 }
