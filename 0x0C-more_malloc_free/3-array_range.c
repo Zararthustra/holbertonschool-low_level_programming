4995ae26a74db4fbcc4b1080574cc71557152d0b
Arthur <arthmayer@outlook.fr>
2020-10-15 13:28:03 +0000

    q3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,5 +10,20 @@
 
 int *array_range(int min, int max)
 {
+	int *a;
+	int i;
 
+	if (min > max)
+		return (NULL);
+	a = malloc((max - min + 1) * sizeof(int));
+	if (a == NULL)
+		return (NULL);
+	i = 0;
+	while (i <= max && min <= max)
+	{
+		a[i] = min;
+		i++;
+		min++;
+	}
+	return (a);
 }
