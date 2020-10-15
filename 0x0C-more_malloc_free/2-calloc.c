0f6cbeeef5ea9e789eee85d523c546feb89150f4
Arthur <arthmayer@outlook.fr>
2020-10-15 12:57:28 +0000

    q2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -35,7 +35,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-	a = malloc((nmemb + size) * sizeof(unsigned int));
+	a = malloc(nmemb * size);
 	if (a == NULL)
 		return (NULL);
 	_memset(a, 0, size * nmemb);
