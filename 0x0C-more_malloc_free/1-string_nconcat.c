164f328a86067db10e83449f98f61a18fb8a3f4d
Arthur <arthmayer@outlook.fr>
2020-10-15 11:30:24 +0000

    q0-q1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,22 @@
 #include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+
+/**
+ * _strlen - returns length of a string
+ * @s: string
+ * Return: length size
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*s++)
+	{
+		a++;
+	}
+	return (a);
+}
 
 /**
  * string_nconcat - cat two strings
@@ -19,8 +34,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1;
 	unsigned int len2;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
