491c7e3c22cbc9df3541760d7b9cc2862e4cb0cd
Arthur <arthmayer@outlook.fr>
2020-10-19 16:44:18 +0000

    q5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age : age
+ * @owner : owner
+ * Return: new_dog, else NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
