93e897650c47b67c01937d58e75cb190f9dabe69
Arthur <arthmayer@outlook.fr>
2020-10-25 20:31:28 +0000

    fix q3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -72,7 +72,7 @@ void print_all(const char * const format, ...)
 
 	va_start(all_format, format);
 	i = 0;
-	while (format[i] != '\0' && format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
 		while (types[j].format != NULL)
