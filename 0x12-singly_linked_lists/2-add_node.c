c829e4fb67ac22dbb0c1cf0acacc07e54b832e8d
Arthur <arthmayer@outlook.fr>
2020-10-29 21:09:02 +0000

    q2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -28,7 +28,6 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	new_node = NULL;
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -36,5 +35,5 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->len = _strlen(str);
 	new_node->next = *head;
 	*head = new_node;
-	return (new_node);
+	return (*head);
 }
