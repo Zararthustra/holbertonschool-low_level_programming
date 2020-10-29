0d76e4e6b8c24343c0fae93f9935ed3583343d0f
Arthur <arthmayer@outlook.fr>
2020-10-29 19:57:37 +0000

    start

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - print all list_t elements
+ * @h: first node
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
