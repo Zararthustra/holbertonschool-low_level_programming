102ad5a55e3959c33dcaf6a233c246ef1ce0ac90
Arthur <arthmayer@outlook.fr>
2020-11-03 14:10:03 +0000

    04 add q7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,11 +13,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *tmp;
 
 	tmp = head;
-	if (tmp == NULL)
-		return (NULL);
 	i = 0;
 	while (i < index)
 	{
+		if (tmp == NULL)
+	 		return (NULL);
 		tmp = tmp->next;
 		i++;
 	}
