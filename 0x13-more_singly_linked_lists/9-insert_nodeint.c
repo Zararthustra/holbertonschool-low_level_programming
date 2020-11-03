59b6cc5d791aac8e406a283fc49ea66ed1281776
Arthur <arthmayer@outlook.fr>
2020-11-03 15:15:50 +0000

    06 add q9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	while (i < idx - 1)
+	while (i < idx - 1 && node != NULL)
 	{
 		node = node->next;
 		++i;
