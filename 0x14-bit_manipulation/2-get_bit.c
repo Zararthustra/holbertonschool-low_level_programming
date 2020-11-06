b147866cf54193c34e393bf208ab955199cff744
Arthur <arthmayer@outlook.fr>
2020-11-06 18:00:05 +0000

    01 fixes

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,14 +11,12 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int res;
 
-	if (index < 0)
+	if (index > 64)
 		return (-1);
-	res = n >> index;
 
+	res = n >> index;
 	if (res & 1)
 		return (1);
-	else if (res | 1)
-		return (0);
 	else
-		return (-1);
+		return (0);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,8 +9,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
-		return (-1);
-	*n = (*n | (1 << index));
-	return (*n);
+	if (index <= 63)
+	{
+		*n = (*n | (1 << index));
+		return (1);
+	}
+	return (-1);
 }
