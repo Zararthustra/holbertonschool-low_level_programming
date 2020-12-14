5b531d4f050c0ff3760b3731648c0c2428b3a919
Arthur <arthmayer@outlook.fr>
2020-12-14 14:21:40 +0000

    fix q7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,13 +11,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new;
 	dlistint_t *back;
+	unsigned int len;
 	unsigned int i;
 
+	len = dlistint_len(*h);
+	if (len < idx)
+		return (NULL);
 	back = *h;
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	if (idx == 0)
+	else if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
 		return (new);
@@ -39,4 +43,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	return (NULL);
 
+}
+
+/**
+ * dlistint_len - returns the number of elements in list
+ * @h: head
+ * Return: number of elem
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	i = 0;
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	while (h)
+	{
+		h = h->prev;
+		i++;
+	}
+	return (i);
 }
